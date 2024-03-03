49bac5fe5a7e97abe3745fcdbce50e82f73a05f6
Hare_The_Coder <120263547+Harecharan-Developer@users.noreply.github.com>
2024-03-03 11:50:45 +0530

    Commit

--- SupabaseTrial/main.py
+++ SupabaseTrial/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from decouple import config
 from supabase import create_client, Client 
-
+import uvicorn
 url = config("SUPERBASE_URL")
 key = config("SUPERBASE_KEY")
 
@@ -16,3 +16,7 @@ def root():
 def get_id():
     data = supabase.table("ID").select("*").execute()
     return data
+
+if __name__ == "__main__":
+        uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
